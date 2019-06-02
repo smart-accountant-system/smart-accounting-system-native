f0cfc418253314c42fc4619628461d5996e387cc
Quang Truong <nhatquangsin@gmail.com>
2019-06-02 15:40:03 +0700

    update eslint config

--- screens/Login.js
+++ screens/Login.js
@@ -1,11 +1,23 @@
+/* eslint-disable react/destructuring-assignment */
 import React, { Fragment } from 'react';
 import { connect } from 'react-redux';
 import { withTheme, Snackbar, Button } from 'react-native-paper';
-import { Text, View, TextInput, ScrollView, KeyboardAvoidingView } from 'react-native';
+import {
+  Text,
+  View,
+  TextInput,
+  ScrollView,
+  KeyboardAvoidingView,
+} from 'react-native';
 
 import { login } from '../actions';
 import Layout from '../constants/Layout';
-import { LoginScreenWrapper, FooContainer, TextInputWrapper, AmazingCircle } from '../containers/Login'
+import {
+  LoginScreenWrapper,
+  FooContainer,
+  TextInputWrapper,
+  AmazingCircle,
+} from '../containers/Login';
 
 class Login extends React.Component {
   state = {
@@ -18,14 +30,17 @@ class Login extends React.Component {
     const { username, password } = this.state;
     const { info, navigation } = this.props;
 
-    this.props.login({ username, password }, {
+    this.props.login(
+      { username, password },
+      {
         success: () => {
           navigation.navigate('Home');
         },
         failure: () => {
           this.setState({ visible: true });
         },
-    });
+      }
+    );
   };
 
   render() {
@@ -33,66 +48,79 @@ class Login extends React.Component {
     const { theme, info } = this.props;
     return (
       <ScrollView>
-
         <KeyboardAvoidingView
-            enabled
-            behavior="padding"
-            keyboardVerticalOffset={0}
+          enabled
+          behavior="padding"
+          keyboardVerticalOffset={0}
+          style={{
+            flex: 1,
+            width: Layout.deviceWidth,
+            height: Layout.deviceHeight,
+          }}
+        >
+          <LoginScreenWrapper>
+            <FooContainer />
+            <AmazingCircle backgroundColor={theme.colors.primary}>
+              <Text
+                style={{
+                  color: theme.colors.whiteText,
+                  fontSize: 20,
+                  fontWeight: 'bold',
+                }}
+              >
+                SAS
+              </Text>
+            </AmazingCircle>
+          </LoginScreenWrapper>
+
+          <View
             style={{
-              flex: 1,
-              width: Layout.deviceWidth,
-              height: Layout.deviceHeight,
+              justifyContent: 'center',
+              alignItems: 'center',
+              paddingTop: 40,
             }}
           >
-            <LoginScreenWrapper>
-              <FooContainer />
-              <AmazingCircle backgroundColor={theme.colors.primary}><Text style={{ color: theme.colors.whiteText, fontSize: 20, fontWeight: 'bold' }}>SAS</Text></AmazingCircle>
-            </LoginScreenWrapper>
+            <TextInputWrapper
+              name="username"
+              placeholder="Username"
+              value={username}
+              autoCapitalize="none"
+              onChangeText={text => this.setState({ username: text })}
+            />
+            <TextInputWrapper
+              placeholder="Password"
+              value={password}
+              autoCapitalize="none"
+              secureTextEntry
+              onChangeText={text => this.setState({ password: text })}
+            />
+            <Text />
 
-            <View style={{ justifyContent: 'center', alignItems: 'center', paddingTop: 40 }}>
-
-              <TextInputWrapper
-                name="username"
-                placeholder="Username"
-                value={username}
-                autoCapitalize="none"
-                onChangeText={text => this.setState({ username: text })}
-              />
-              <TextInputWrapper
-                placeholder="Password"
-                value={password}
-                autoCapitalize="none"
-                secureTextEntry
-                onChangeText={text => this.setState({ password: text })}
-              />
-              <Text></Text>
-
-               <Button 
-                mode="contained" 
-                style={{ width: Layout.deviceWidth - 50 }}
-                onPress={this.handleLogin}
-                loading={info.isLogging}
-              >
-                Login
-              </Button>
-            </View>
-
-            <Snackbar
-              visible={visible}
-              onDismiss={() => this.setState({ visible: false })}
-              style={{
-                backgroundColor: theme.colors.primary,
-                color: theme.colors.whiteText,
-              }}
+            <Button
+              mode="contained"
+              style={{ width: Layout.deviceWidth - 50 }}
+              onPress={this.handleLogin}
+              loading={info.isLogging}
             >
-              Tên đăng nhập hoặc mật khẩu không hợp lệ
-            </Snackbar>
-          </KeyboardAvoidingView>
-        </ScrollView>
+              Login
+            </Button>
+          </View>
+
+          <Snackbar
+            visible={visible}
+            onDismiss={() => this.setState({ visible: false })}
+            style={{
+              backgroundColor: theme.colors.primary,
+              color: theme.colors.whiteText,
+            }}
+          >
+            Tên đăng nhập hoặc mật khẩu không hợp lệ
+          </Snackbar>
+        </KeyboardAvoidingView>
+      </ScrollView>
     );
   }
 }
-
 
 const mapStateToProps = state => ({
   info: state.user,
@@ -101,4 +129,9 @@ const mapDispatchToProps = {
   login,
 };
 
-export default withTheme(connect(mapStateToProps, mapDispatchToProps)(Login));
+export default withTheme(
+  connect(
+    mapStateToProps,
+    mapDispatchToProps
+  )(Login)
+);

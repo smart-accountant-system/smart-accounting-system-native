6ee99aa5587875e861810a98c27b149fc81268bf
DukeThor <ducthotran2010@gmail.com>
2019-06-08 14:39:25 +0700

    Apply date filter for Receipt

--- redux/actions/receipt.js
+++ redux/actions/receipt.js
@@ -6,7 +6,7 @@ export const GET_RECEIPTS_REQUEST = 'get-receipts-request';
 export const GET_RECEIPTS_SUCCESS = 'get-receipts-success';
 export const GET_RECEIPTS_FAILURE = 'get-receipts-failure';
 
-export function getReceipts(callback) {
+export function getReceipts({ params, success, failure }) {
   return async dispatch => {
     try {
       dispatch({ type: GET_RECEIPTS_REQUEST });
@@ -14,23 +14,24 @@ export function getReceipts(callback) {
 
       const result = await query({
         endpoint,
+        params,
         method: METHODS.get,
       });
 
       if (result.status === 200) {
-        callback.success();
+        success();
         dispatch({
           type: GET_RECEIPTS_SUCCESS,
           payload: result.data,
         });
       } else {
-        callback.failure();
+        failure();
         dispatch({
           type: GET_RECEIPTS_FAILURE,
         });
       }
     } catch (error) {
-      callback.failure();
+      failure();
       dispatch({
         type: GET_RECEIPTS_FAILURE,
         payload: error,
--- screens/Receipt.js
+++ screens/Receipt.js
@@ -1,27 +1,107 @@
 /* eslint-disable react/destructuring-assignment */
 import React from 'react';
-import { View, TouchableOpacity, ScrollView } from 'react-native';
+import {
+  Text,
+  View,
+  TouchableOpacity,
+  ScrollView,
+  Animated,
+} from 'react-native';
 import { connect } from 'react-redux';
-import { withTheme } from 'react-native-paper';
+import DateTimePicker from 'react-native-modal-datetime-picker';
+import { Button, withTheme } from 'react-native-paper';
 import moment from 'moment';
 
 import i18n from 'i18n-js';
 import { getReceipts } from '../redux/actions';
 
-import {
-  HeaderWrapper,
-  Header,
-  Typography,
-  HomeBodyWrapper,
-} from '../containers/Home';
+import { HeaderWrapper, Header, Typography } from '../containers/Home';
 import theme from '../constants/theme';
 import FeatherIcon from '../components/FeatherIcon';
 import ReceiptItem from '../components/ReceiptItem';
 import Loading from '../components/Loading';
 
+import {
+  FilterHeader,
+  FilterBody,
+  FilterField,
+  FilterTime,
+} from '../components/Filter';
+
 class Receipt extends React.Component {
+  state = {
+    isDatePickerVisible: false,
+    activatingDate: undefined,
+    fromDate: new Date(),
+    toDate: new Date(),
+
+    isExpandingFilter: false,
+    filterHeight: new Animated.Value(0),
+  };
+
   componentDidMount = () => {
     this.props.getReceipts({
+      success: () => {},
+      failure: () => {},
+    });
+  };
+
+  showDateTimePicker = activatingDate => {
+    this.setState({ isDatePickerVisible: true, activatingDate });
+  };
+
+  hideDateTimePicker = () => {
+    this.setState({ isDatePickerVisible: false });
+  };
+
+  handleDatePicked = date => {
+    const { activatingDate } = this.state;
+    if (activatingDate === 'From') {
+      this.setState({
+        fromDate: new Date(date),
+      });
+    } else {
+      this.setState({
+        toDate: new Date(date),
+      });
+    }
+    this.hideDateTimePicker();
+  };
+
+  handlePressFilter = () => {
+    const { isExpandingFilter, filterHeight } = this.state;
+
+    if (!isExpandingFilter) {
+      Animated.spring(filterHeight, {
+        toValue: 135,
+      }).start();
+    } else {
+      Animated.timing(filterHeight, {
+        toValue: 0,
+      }).start();
+    }
+
+    this.setState({
+      isExpandingFilter: !isExpandingFilter,
+    });
+  };
+
+  doFilter = () => {
+    const { filterHeight, fromDate, toDate } = this.state;
+
+    Animated.timing(filterHeight, {
+      toValue: 0,
+    }).start();
+
+    this.setState({
+      isExpandingFilter: false,
+    });
+
+    this.props.getReceipts({
+      params: {
+        startDate: new Date(fromDate.toDateString()),
+        endDate: new Date(toDate.toDateString()),
+      },
       success: () => {},
       failure: () => {},
     });
@@ -34,6 +114,12 @@ class Receipt extends React.Component {
 
   render() {
     const { receipts } = this.props;
+    const {
+      isDatePickerVisible,
+      fromDate,
+      toDate,
+      activatingDate,
+    } = this.state;
     return (
       <View style={{ display: 'flex', flex: 1 }}>
         <HeaderWrapper>
@@ -43,25 +129,61 @@ class Receipt extends React.Component {
             <FeatherIcon color={theme.colors.primary} name="user" />
           </Header>
         </HeaderWrapper>
+
+        <FilterHeader
+          title="Advance filter"
+          isExpand={this.state.isExpandingFilter}
+          onPress={this.handlePressFilter}
+        />
+
+        <FilterBody height={this.state.filterHeight}>
+          <FilterField first>
+            <FilterTime
+              title="From"
+              first
+              date={fromDate.toLocaleDateString('vi-VN')}
+              showDateTimePicker={this.showDateTimePicker}
+            />
+            <FilterTime
+              title="To"
+              second
+              date={toDate.toLocaleDateString('vi-VN')}
+              showDateTimePicker={this.showDateTimePicker}
+            />
+          </FilterField>
+
+          <FilterField height="52">
+            <FeatherIcon color="#f1f1f1" name="user" />
+            <Button mode="contained" onPress={this.doFilter}>
+              <Text style={{ color: theme.colors.white }}>Filter</Text>
+            </Button>
+          </FilterField>
+        </FilterBody>
+
+        <DateTimePicker
+          isVisible={isDatePickerVisible}
+          date={activatingDate === 'from' ? fromDate : toDate}
+          onConfirm={this.handleDatePicked}
+          onCancel={this.hideDateTimePicker}
+        />
+
         {receipts ? (
-          <HomeBodyWrapper>
-            <ScrollView>
-              {receipts.receipts.map(item => (
-                <TouchableOpacity
-                  onPress={() => this.receiptDetail(item)}
-                  key={item._id}
-                >
-                  <ReceiptItem
-                    customer={item.customer.name}
-                    type={item.type}
-                    date={moment(item.createdAt).format('MMM DD, YYYY')}
-                    item={item}
-                    price={item.payment.amountMoney}
-                  />
-                </TouchableOpacity>
-              ))}
-            </ScrollView>
-          </HomeBodyWrapper>
+          <ScrollView>
+            {receipts.receipts.map(item => (
+              <TouchableOpacity
+                onPress={() => this.receiptDetail(item)}
+                key={item._id}
+              >
+                <ReceiptItem
+                  customer={item.customer.name}
+                  type={item.type}
+                  date={moment(item.createdAt).format('MMM DD, YYYY')}
+                  item={item}
+                  price={item.payment.amountMoney}
+                />
+              </TouchableOpacity>
+            ))}
+          </ScrollView>
         ) : (
           <Loading />
         )}

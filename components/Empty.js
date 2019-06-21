1cfd4bc1c7143598e3e95b05b118d80fa53e01ce
Quang Truong <nhatquangsin@gmail.com>
2019-06-21 16:56:23 +0700

    add empty picture

--- components/Empty.js
+++ components/Empty.js
@@ -1,14 +1,20 @@
 import React from 'react';
 import i18n from 'i18n-js';
-import { View, Text } from 'react-native';
 import styled from 'styled-components';
+
+import empty from '../assets/empty.png';
 
 const NiceView = styled.View`
   display: flex;
   align-items: center;
   justify-content: flex-end;
 
-  height: 200px;
+  height: 400px;
+`;
+
+const EmptyPicture = styled.Image`
+  width: 300px;
+  height: 300px;
 `;
 
 const EmptyText = styled.Text`
@@ -16,6 +22,7 @@ const EmptyText = styled.Text`
 `;
 export default ({ name }) => (
   <NiceView>
+    <EmptyPicture source={empty} />
     <EmptyText>
       {i18n.t('messageEmpty')} {name.toLowerCase()}.
     </EmptyText>
